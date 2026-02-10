5da452f9ad94ec100f0a9aaf54d7d96aab84cdca
ft-website-prep <github.ft@gmail.com>
2026-02-10 18:58:48 +0100

    push

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Website Playground</h1>
+      <p>Starten Sie den Dev-Server mit <code>npm run dev</code>.</p>
+    </div>
+  )
+}
